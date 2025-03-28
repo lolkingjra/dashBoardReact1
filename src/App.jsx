8b5f240d02e5734e972921b4111c9d46a2ab2a4e
lolkingjra <ja2159586@gmail.com>
2025-03-27 20:03:32 -0600

    Initial commit

---
+++ src/App.jsx
@@ -0,0 +1,20 @@
+import { useState } from 'react'
+import reactLogo from './assets/react.svg'
+import viteLogo from '/vite.svg'
+import './App.css'
+import Card from './components/card';
+
+function App() {
+  return (
+    <div className="App">
+
+      <h1 className='text-center text-3xl'>¡Tailwind está funcionando! 🎉</h1>
+      <Card/>
+    </div>
+  );
+
+
+    
+}
+
+export default App
